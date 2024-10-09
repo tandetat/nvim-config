1b1462b9ceb6fce62d77a0a8dad57d0c86d7111f
Diogo Tandeta Tartarotti <tandetat@grinnell.edu>
2024-10-09 14:23:26 -0300

    modularize config

---
+++ lua/kickstart/plugins/tokyonight.lua
@@ -0,0 +1,31 @@
+return {
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Require the custom get_style module
+      local get_style = require 'lua.custom.get_style'
+
+      -- Call the get_appearance function
+      local style = get_style.get_appearance()
+      require('tokyonight').setup {
+        -- use the night style
+        style = style,
+        transparent = 'true',
+        on_colors = function(colors) end,
+        on_highlights = function(highlights, colors) end,
+      }
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
