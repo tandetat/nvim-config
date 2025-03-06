d3592ce114584e86f3035e5f7c2745d093fc449c
Diogo Tandeta Tartarotti <tandetat@grinnell.edu>
2025-03-06 15:03:52 -0300

    change latex rendering in render-markdown.nvim

--- lua/custom/plugins/render-markdown.lua
+++ lua/custom/plugins/render-markdown.lua
@@ -1,9 +1,21 @@
 return {
+  { 'jbyuki/nabla.nvim', event = 'VeryLazy' },
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    lazy = true,
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    event = 'VeryLazy',
     ft = 'markdown',
-    opts = {},
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    opts = {
+      latex = { enabled = false },
+      win_options = { conceallevel = { rendered = 2 } },
+      on = {
+        render = function()
+          require('nabla').enable_virt { autogen = true }
+        end,
+        clear = function()
+          require('nabla').disable_virt()
+        end,
+      },
+    },
   },
 }
