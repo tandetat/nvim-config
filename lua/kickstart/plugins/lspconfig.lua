26ee12d965bbf4e7add8d6792b31dd220be1d957
Diogo Tandeta Tartarotti <tandetat@grinnell.edu>
2025-05-01 02:22:29 -0300

    fix: refactor lazydev, remove luvit meta

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -1,5 +1,17 @@
 return {
   {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
     dependencies = {
