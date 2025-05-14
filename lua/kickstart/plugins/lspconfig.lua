cc87a4d959d836ab237fc0ebaebf12d8c267926d
Diogo Tandeta Tartarotti <tandetat@grinnell.edu>
2025-05-14 13:58:11 -0300

    fix: merge upstreamm kickstart changes

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -17,8 +17,8 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependants
-      { 'williamboman/mason-lspconfig.nvim' },
+      { 'mason-org/mason.nvim', opts = {} }, -- NOTE: Must be loaded before dependants
+      { 'mason-org/mason-lspconfig.nvim' },
       { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
 
       -- Useful status updates for LSP.
